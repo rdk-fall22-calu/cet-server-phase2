6630464a5fd404562c2c55fad7ef0a5841864e54
Robert Krency <bobkrency@gmail.com>
2022-10-27 17:20:12 -0400

    debug

--- src/server.c
+++ src/server.c
@@ -195,10 +195,11 @@ void *connection_handler(void *clientInfo)
 		
 		//Send the message back to client
         write(sock , message , strlen(message));
+        snprintf(displayMessage, sizeof(displayMessage), "Message being sent: \n\n\t %s \n\n", message);
+        log_message(threadName, displayMessage);
 		
 		//clear the message buffer
 		memset(client_message, 0, 4000);
-        message = NULL;
 
         // Disconnect
         if (quit == 1)
