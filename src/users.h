cbf6ef7c04f95a1cca4727e1960f4e4905c0ca3d
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-03 17:07:10 -0400

    Update users.h

--- src/users.h
+++ src/users.h
@@ -1,6 +1,8 @@
 /**
  * @file users.h
- * @author 
+ * @author Distint Howie (how4685@pennwest.edu)
+ * @author Robert Krency (kre1188@pennwest.edu)
+ * @author Anthony Stepich (ste4864@pennwest.edu)
  * @brief Defines the registered users 
  * 
  */
