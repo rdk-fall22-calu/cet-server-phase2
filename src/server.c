bc44957ea67fd4959b19aa88a1d571bb8cf773e0
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-03 17:06:06 -0400

    Update server.c

--- src/server.c
+++ src/server.c
@@ -1,4 +1,8 @@
-
+/*
+ *@author Distint Howie (how4685@pennwest.edu)
+ * @author Robert Krency (kre1188@pennwest.edu)
+ * @author Anthony Stepich (ste4864@pennwest.edu)
+*/
 
 #include <errno.h>
 #include <stdlib.h>
