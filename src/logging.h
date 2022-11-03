20ff01b2f81b02f55375b69cd427cddee99756f7
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-03 17:05:25 -0400

    Update logging.h

--- src/logging.h
+++ src/logging.h
@@ -1,4 +1,8 @@
-
+/*
+  *@author Distint Howie (how4685@pennwest.edu)
+ * @author Robert Krency (kre1188@pennwest.edu)
+ * @author Anthony Stepich (ste4864@pennwest.edu)
+*/
 
 #ifndef __LOGGING_H__
 #define __LOGGING_H__
