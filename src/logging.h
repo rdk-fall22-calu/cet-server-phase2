0048aec273660a3efdf3a702fbae36949533ce1e
Distint <113930726+Distint@users.noreply.github.com>
2022-11-03 17:01:45 -0400

    Update logging.h

--- src/logging.h
+++ src/logging.h
@@ -3,6 +3,6 @@
 #ifndef __LOGGING_H__
 #define __LOGGING_H__
 
-void log_message(char *sender, char *message);
+void log_message(char *sender, char *message); //Server messages to terminal
 
 #endif __LOGGING_H__
