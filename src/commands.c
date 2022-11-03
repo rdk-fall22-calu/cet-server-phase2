ff3f724b3e4d4868f69299519bbc7c4b1695950e
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-03 17:02:29 -0400

    Update commands.c
    docu changes

--- src/commands.c
+++ src/commands.c
@@ -1,6 +1,8 @@
 /**
  * @file commands.c
- * @author 
+ *  @author Distint Howie (how4685@pennwest.edu)
+ * @author Robert Krency (kre1188@pennwest.edu)
+ * @author Anthony Stepich (ste4864@pennwest.edu)
  * @brief 
  * 
  */
