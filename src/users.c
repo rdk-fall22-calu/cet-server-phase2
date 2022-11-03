74c8f058882a7d7c14ab76e34e40472c723033b0
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-03 17:06:43 -0400

    Update users.c

--- src/users.c
+++ src/users.c
@@ -1,6 +1,8 @@
 /**
  * @file users.c
- * @author 
+ * @author Distint Howie (how4685@pennwest.edu)
+ * @author Robert Krency (kre1188@pennwest.edu)
+ * @author Anthony Stepich (ste4864@pennwest.edu)
  * @brief 
  * 
  */
